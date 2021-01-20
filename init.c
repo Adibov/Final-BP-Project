26da5e4e504544aa29053c213416b938c93e549a
Amirhesam Adibinia <hesamadibi80@gmail.com>
2021-01-20 16:19:28 +0330

    Update init.c

--- init.c
+++ init.c
@@ -6,16 +6,34 @@
 #include <conio.h>
 #include <dirent.h>
 #include <errno.h>
+#include <unistd.h>
 
 /* functions declrations */
 void init(void);
 void check_files(void);
+void check_binary_files(void);
 
 /* functions definitions */
 void init() {
 	check_files();
+	check_binary_files();
 }
 
 void check_files() {
-	
+	DIR* dir = opendir(".\\Files");
+	if (dir)  /* Directory exists. */
+		closedir(dir);
+	else if (ENOENT == errno) /* Directory does not exist. */
+		system("mkdir .\\Files");
+	else {
+		/* opendir() failed for some other reason. */
+		system("CLS");
+		printf("Some error occurred :(");
+		exit(-1);
+	}
+}
+
+void check_binary_files() {
+	if (access("Files\\Users.bin", F_OK)) // file doesn't exists
+		system("touch Files\\Users.bin");
 }
